a3e3f78f326684ebd14b615a86cfa2d32af47990
Manish270698 <manishjune27@gmail.com>
2023-04-21 22:16:05 +0530

    simple todo list

---
+++ TODO List/app.js
@@ -0,0 +1,63 @@
+var arr = [];
+
+function submit() {
+    let title = document.querySelector('.title').value;
+    let desc = document.querySelector('.description').value;
+    if (localStorage.getItem('itemsJSON') == null) {
+        arr.push([title, desc]);
+        localStorage.setItem('itemsJSON', JSON.stringify(arr));
+    }
+    else {
+        itemsStr = localStorage.getItem('itemsJSON');
+        arr = JSON.parse(itemsStr);
+        arr.push([title, desc]);
+        localStorage.setItem('itemsJSON', JSON.stringify(arr));
+    }
+    update();
+}
+
+function clearStore() {
+    localStorage.clear();
+    arr = [];
+    update();
+}
+
+function update() {
+    let tableBody = document.getElementById('tableBody');
+    let str = "";
+    if (localStorage.getItem('itemsJSON') !== null) {
+        let itemsStr = localStorage.getItem('itemsJSON');
+        arr = JSON.parse(itemsStr);
+
+        // Populating the table
+        arr.forEach((element, index) => {
+            str += `
+            <tr>
+                <th scope="row">${index + 1}</th>
+                <td>${element[0]}</td>
+                <td>${element[1]}</td>
+                <td><button type="button" class="btn btn-outline-danger" onclick="deleteMe(${index})">Delete</button></td>
+            </tr>`
+        })
+    }
+    tableBody.innerHTML = str;
+}
+
+add = document.querySelector('.add');
+add.addEventListener('click', submit);
+
+
+clearAll = document.querySelector('.clearAll');
+clearAll.addEventListener('click', function () {
+    if (confirm('Do you really want to clear the list?')) {
+        clearStore();
+    }
+});
+
+function deleteMe(index) {
+    arr.splice(index, 1);
+    localStorage.setItem('itemsJSON', JSON.stringify(arr));
+    update();
+}
+
+window.onload = update();
